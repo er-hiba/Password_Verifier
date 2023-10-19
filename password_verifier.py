84f9b2facc22ebe3c1c84f4c67c84751c595518c
Hiba ERRAOUI <125480922+er-hiba@users.noreply.github.com>
2023-10-19 02:07:14 +0100

    :)

---
+++ password_verifier.py
@@ -0,0 +1,11 @@
+password = input("Enter the password: ")
+i = 1
+while password != "DEV@2023" and i < 3:
+        password = input("Reenter the password: ")
+        i += 1
+if password == "DEV@2023":
+    print("Hello")
+
+if i == 3 and password != "DEV@2023"  :
+    x = input("What's your favorite color? ")
+
